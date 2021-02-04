da33a9cf1554597a716afd9b000ff19ae24bfde3
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-04 11:35:15 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,16 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Aranthangi" }, "geometry": { "type": "Point", "coordinates": [ 78.996516464859113, 10.162585928719571, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thirumayam" }, "geometry": { "type": "Point", "coordinates": [ 78.752201854874727, 10.246779531188309, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Pudukkottai" }, "geometry": { "type": "Point", "coordinates": [ 78.819776608986103, 10.38406964862976, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Viralimalai" }, "geometry": { "type": "Point", "coordinates": [ 78.54516916870341, 10.603860366837489, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Alangudi" }, "geometry": { "type": "Point", "coordinates": [ 78.980225426947555, 10.36194340070104, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Gandharvakottai" }, "geometry": { "type": "Point", "coordinates": [ 79.013466367032009, 10.57273679363942, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Ponnnamaravathi" }, "geometry": { "type": "Point", "coordinates": [ 78.536778489088022, 10.282412757061451, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Keeranur" }, "geometry": { "type": "Point", "coordinates": [ 78.783106233987439, 10.57263013865485, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Manamelkudi" }, "geometry": { "type": "Point", "coordinates": [ 79.232303207519976, 10.04159853544258, -999999.0 ] } }
+]
+}
---
+++ data/VillageWiseFloodVulnerbality_5.js
@@ -0,0 +1,8 @@
+var json_VillageWiseFloodVulnerbality_5 = {
+"type": "FeatureCollection",
+"name": "VillageWiseFloodVulnerbality_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 78.873310521345743, 10.386308474513271 ] } }
+]
+}
